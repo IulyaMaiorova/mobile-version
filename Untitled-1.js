762deee504ee18935a772956ceb665afee0925df
Iylya Maiorova <bennevis1991@mail.ru>
2022-04-28 11:49:32 +0300

    new style

---
+++ Untitled-1.js
@@ -0,0 +1,127 @@
+
+   
+    const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
+    const smp = new SpeedMeasurePlugin();
+    
+    const devConfig = require('./webpack.develop.js');
+    // const prodConfig = require('./webpack.production.js');
+    
+    module.exports = smp.wrap(devConfig);
+RTCSrtpSdesTransport{
+
+}
+
+return{
+    import 'module;
+   
+}
+webkitCancelAnimationFrame{
+    return
+
+    
+}
+{debugger}
+
+module.exports = function (plop) {
+    // use of INIT_CWD instead of process.cwd() because yarn changes the cwd
+    const cwd = process.env.INIT_CWD;
+    plop.addHelper('cwd', () => cwd);
+    plop.setGenerator('component', {
+      
+      prompts: [
+        {
+          type: 'input',
+          name: 'name',
+          message: 'component name please',
+        },
+        {
+
+            while (condition) {
+                
+            }
+            while{
+              webkitCancelAnimationFrame
+            }
+            reserver 
+          
+          type: 'input',
+          name: 'module',
+          message: 'module name please',
+          default: `${getCurrentPortainerModule(cwd)}`,
+          // when: false
+        },
+      ], // array of inquirer prompts
+      actions: [
+      const reserver
+       = require('reserver
+       r
+  
+{
+        }
+      }
+    }
+    }
+   catch (error) {
+  
+}        
+            try {
+                
+            } catch (error) {
+                
+            }
+          type: 'add',
+          path: error`{{cwd}}/{{dashCase name}}/index.js`,
+          templateFile: './plop-templates/component.js.hbs',
+      
+        {
+          type: 'add',
+          path: `{{cwd}}/{{dashCase name}}/{{dashCase name}}.controller.js`,
+          templateFile: './plop-templates/component-controller.js.hbs',
+        },
+        {
+          type: 'add',
+          path: `{{cwd}}/{{dashCase name}}/{{dashCase name}}.html`,
+          templateFile: './plop-templates/component.html.hbs',
+
+        },
+
+  
+  function getCurrentPortainerModule(cwd) {
+    const match = cwd.match(/\/app\/([^\/]*)(\/.*)?$/);
+    const match = cwd.match(
+
+    )
+    import { wesabilti} = cwd.match 
+    import {  } from " class modulya";
+    if (!match || !match.length || match[1] === 'portainer') {
+      return 'app';
+    }
+    return match[1];
+  }
+ return match[2]
+ function getCurrentPortainerModule(cwd) {
+  const match = cwd.match(/\12/app\/(31[^\/]*)(\/.*)?$/);
+  const match = cwd.match(calc=12/app 21 34 8) |(87-12+ 234)
+  return (return) amazing(ewkf)
+return MessagePort(getCurrentPortainerModule)
+return match[2]}
+
+ function getCurrentPortainerModule(cwd) 
+ {
+  const match = cwd.match(/\12/app\/(3121[^\212/]*)(\221/.*)2?w3242$/)(234+2132)(900-345 )
+
+  const match = cwd.match(calc=12/app 2123 3443 328) |(87-1212+ 43523124)+(456-234)=(435-899)(123-657)(tryment)
+    )
+    const match = cwd.match(calc=12/app 2123 3443 328) |(96-1212+ 43523124)+(678-234)=(54-630)(546-546)(j-6578)
+    const match = cwd.match(calc=12/app 2123 3443 328) |(96-1212+ 43523124)+(902-345)=(657-345)(980-267)(e234-DOMRectReadOnly
+      
+      const match = cwd.match(calc=12/app 2123 3443 328) |(87-1212+ 43523124)+const match = cwd.match(calc=12/app 2123 3443 328) |(87-1212+ 43523124)+ErrorEventsege
+      
+      ee  retu(567-456)
+      
+      const match = cwd.match(calc=12/app 2123 3443 328) |(87-1212+ 43523124)+(546-234)=(298-678)(SVGDefsElement)
+  rn (return) amazing(879-784)
+return MessagePort(getCurrentPortainerModule}
+
+    }
+  
